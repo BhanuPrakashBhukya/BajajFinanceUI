b0042bfc0419248bec532879cd07fc98a5fafa53
Bhanu Prakash Bhukya <bhanuprakash@aititude.in>
2024-08-25 12:32:33 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JsonInputComponent } from './json-input/json-input.component';
+import { ResponseDisplayComponent } from './response-display/response-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsonInputComponent,
+    ResponseDisplayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
